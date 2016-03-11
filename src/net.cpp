c3943b7f5197ca7466ba2d80b0639d1d547a5bdb
SoonCoin <info@sooncoin.com>
2016-03-11 14:14:17 +0100

    added seed node see.corgicoin.co.uk

--- src/net.cpp
+++ src/net.cpp
@@ -1285,6 +1285,7 @@ void MapPort()
 static const char *strDNSSeed[][2] = {
     {"82.46.79.119", "82.46.79.119"},
     {"192.52.167.140", "192.52.167.140"},
+    {"seed.corgicoin.co.uk", "seed.corgicoin.co.uk"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
